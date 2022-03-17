86e27fc500a6fc1832f7679e36560916dece635b
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-03-17 14:59:58 -0700

    Create board module and functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,52 @@
 // main.js
+const gameBoard = (() =>
+{
+    const board = [ [" ", " ", " "], 
+                    [" ", " ", " "], 
+                    [" ", " ", " "] ];
+    const printboard = () =>
+    {
+        for (let i = 0; i < board.length; i++)
+        {
+            let line = "";
+            for (let j = 0; j < board[i].length; j++)
+            {
+                line += "[ " + board[i][j] + " ]";
+            }
+            console.log(line);
+            if (i < 2)
+            {
+                console.log('---------------');
+            }
+        }
+    };
+
+    const getMark = (row, column) =>
+    {
+        return board[row][column];
+    }
+    
+    const setMark = (row, column, symbol) =>
+    {
+        board[row][column] = symbol;
+    }
+
+    const clearBoard = () =>
+    {
+        for (let i = 0; i < board.length; i++)
+        {
+            for (let j = 0; j < board[i].length; j++)
+            {
+                setMark(i, j, " ");
+            }
+        }
+    }
+
+    return {
+        board,
+        printboard,
+        getMark,
+        setMark,
+        clearBoard,
+    }
+})();
