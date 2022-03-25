5ee9fdd2c89761ed308b19fa846e19d6a5ef0a22
rdat5 <42378063+rdat5@users.noreply.github.com>
2022-03-24 23:15:38 -0700

    Set up board marking

--- js/main.js
+++ js/main.js
@@ -45,6 +45,11 @@ const gameBoard = (() =>
         if (getMark(row, column) === null)
         {
             board[row][column] = symbol;
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
@@ -75,3 +80,47 @@ const Player = (name, mark) =>
         mark,
     }
 }
+
+const game = (() =>
+{
+    const allButtons = document.querySelectorAll('.gameboard-wrapper button');
+    const startButton = document.querySelector('.game-start-btn');
+
+    let gameInProgress = false;
+    let player1;
+    let player2;
+    let currentPlayer;
+    
+    // button setup
+    for (let i = 0; i < allButtons.length; i++)
+    {
+        allButtons[i].addEventListener("click", function()
+        {
+            let cellRow = allButtons[i].getAttribute('data-row');
+            let cellCol = allButtons[i].getAttribute('data-col');
+            
+            if (gameInProgress)
+            {
+                if (gameBoard.setMark(cellRow, cellCol, currentPlayer.mark))
+                {
+                    // change turns
+                    console.log("eh");
+                }
+                gameBoard.printboard();
+            }
+        });
+    }
+
+    startButton.addEventListener("click", function()
+    {
+        gameInProgress = true;
+        player1 = Player("ray", symX);
+        player2 = Player("bepis", symO);
+
+        currentPlayer = player2;
+    })
+
+
+    // button setup
+
+})();
